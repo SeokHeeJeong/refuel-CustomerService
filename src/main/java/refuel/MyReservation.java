c1c6a716ec592b5a2995f59c23093d43d28c5771
SeokHeeJeong <vssh77@gmail.com>
2020-10-16 21:22:09 +0900

    보상트랜젝션 추가

--- src/main/java/refuel/MyReservation.java
+++ src/main/java/refuel/MyReservation.java
@@ -1,14 +1,17 @@
 package refuel;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.List;
 
+@Data
 @Entity
 @Table(name="MyReservation_table")
 public class MyReservation {
 
         @Id
-        @GeneratedValue(strategy=GenerationType.AUTO)
+        @GeneratedValue(strategy=GenerationType.IDENTITY)
         private Long id;
         private Long reservationId;
         private String fuelType;
@@ -18,78 +21,6 @@ public class MyReservation {
         private Long price;
         private String paymentType;
         private String paymentStatus;
-        private Integer qty;
-
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-        public Long getReservationId() {
-            return reservationId;
-        }
-
-        public void setReservationId(Long reservationId) {
-            this.reservationId = reservationId;
-        }
-        public String getFuelType() {
-            return fuelType;
-        }
-
-        public void setFuelType(String fuelType) {
-            this.fuelType = fuelType;
-        }
-        public String getCustomerId() {
-            return customerId;
-        }
-
-        public void setCustomerId(String customerId) {
-            this.customerId = customerId;
-        }
-        public String getStationId() {
-            return stationId;
-        }
-
-        public void setStationId(String stationId) {
-            this.stationId = stationId;
-        }
-        public String getReservationStatus() {
-            return reservationStatus;
-        }
-
-        public void setReservationStatus(String reservationStatus) {
-            this.reservationStatus = reservationStatus;
-        }
-        public Long getPrice() {
-            return price;
-        }
-
-        public void setPrice(Long price) {
-            this.price = price;
-        }
-        public String getPaymentType() {
-            return paymentType;
-        }
-
-        public void setPaymentType(String paymentType) {
-            this.paymentType = paymentType;
-        }
-        public String getPaymentStatus() {
-            return paymentStatus;
-        }
-
-        public void setPaymentStatus(String paymentStatus) {
-            this.paymentStatus = paymentStatus;
-        }
-        public Integer getQty() {
-            return qty;
-        }
-
-        public void setQty(Integer qty) {
-            this.qty = qty;
-        }
+        private int qty;
 
 }
